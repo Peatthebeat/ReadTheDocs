87f6de022852e582e5a46091f9e40af243adf8d1
Pierre-Luc Morais <pierrelucmorais@gmail.com>
2020-08-28 22:59:05 -0400

    Changing Theme

--- source/conf.py
+++ source/conf.py
@@ -27,8 +27,9 @@ author = 'Pierre-Luc Morais'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+import sphinx_rtd_theme
+
+extensions = ['sphinx_rtd_theme']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -44,7 +45,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -53,3 +54,4 @@ html_static_path = ['_static']
 
 master_doc = 'index'
 # https://stackoverflow.com/questions/56336234/build-fail-sphinx-error-contents-rst-not-found
+
